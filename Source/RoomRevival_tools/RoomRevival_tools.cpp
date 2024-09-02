59721a23350027d6ad1c74bebbfad6a1a96ba52d
razorfield <Razorfield@mail.ru>
2024-09-02 22:04:47 +0300

    RestFiles2

---
+++ Source/RoomRevival_tools/RoomRevival_tools.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoomRevival_tools.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoomRevival_tools, "RoomRevival_tools" );
